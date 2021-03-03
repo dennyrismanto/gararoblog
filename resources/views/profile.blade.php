91531b44137032919eb6f42df36a63880b18dde8
dennyrismanto <rismanto.denny@gmail.com>
2021-03-03 17:41:18 +0700

    Segarkan

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Curriculum vitae</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="assets/styles/style.css">
+</head>
+
+
+<body>
+
+
+<header>
+    <div id="jumbotron">
+        <h1 class="ml1">
+            <span class="text-wrapper">
+              <span class="line line1"></span>
+              <span class="letters">Curriculum vitae</span>
+              <span class="line line2"></span>
+            </span>
+          </h1>
+          
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+    </div>
+</header>
+
+<main>
+
+<div id="content">
+<article class="profile img" id="card">
+    <header>
+        <h2 id="jarak">Biodata</h2>
+        <figure>
+            <img src="assets/photo.jpeg">
+        </figure>
+        <h2 id="Nama">Denny Rismanto</h2>
+        <!-- <h4 id="Profile">Honest, Professional, Sociable</h4> -->
+    </header>
+
+    <section>
+        <table>
+            <tr id="Kiri">
+                <th>Alamat</th>
+            </tr>
+            <tr>
+            <td>Jl.Kemiri IX No.79, Pondok cabe udik,
+                Pamulang, Tangerang selatan, Banten</td>
+            </tr>
+
+            <tr id="Kiri">
+                <th>Education</th>
+            </tr>
+            <tr>
+                <td>Bachelor of Education, Physics Education-UIN Syarif Hidayatullah Jakarta</td>
+            </tr>
+
+           <tr id="Kiri">
+                <th>Email</th>
+           </tr>
+           <tr>
+                <td>rismanto.denny@gmail.com</td>
+           </tr>
+           
+            <tr id="Kiri">
+                <th>Nomor Telepon/HP</th>
+            </tr>
+            <tr>
+                <td>+62 899 9758 898</td>
+            </tr>
+
+            <tr id="Kiri">
+                <th>Github</th>
+            </tr>
+            <tr>
+                <td>https://github.com/dennyrismanto</td>
+            </tr>
+       </table>
+     </section>
+</article>
+
+<canvas id="canvas" width="200px" height="200px" style="background-color:#333"> </canvas>
+
+</div>
+
+
+<aside>
+    <article id="card">
+
+        <h3>Certificate</h3>
+        <h4>Kementerian Hukum dan Hak Asasi Manusia</h4>
+        <table>
+            <tr>15 August 2018</tr>
+            <p> Developing science learning applications for junior high school student as a creator. Surat Pencatatan Ciptaan on number EC00201823904</p>
+        </table>
+        
+        <h4>Google Developers</h4>
+        <table>
+            <tr>December 2017 - February 2018</tr>
+            <p>Training to develop Android Basic: User Interface & Android Basic: User Input as a participant</p>
+        </table>
+
+        <h4>Mumtaza Islamic School</h4>
+        <table>
+            <tr>18 August 2017</tr>
+            <p>Contributions to the Independance & Science Day as a facilitator</p>
+        </table>
+
+        <h4>Universitas Islam Negeri Syarif Hidayatullah</h4>
+        <table>
+            <tr>14 September 2014</tr>
+            <p>Contributions to the National seminar on science education as a moderator</p>
+        </table>
+
+        <h4>Dicoding(Google Developers Kejar 2019)</h4>
+        <table>
+            <tr>21 June 2019 - 5 October 2019</tr>
+            <p>Training to develop the Native Android Mobile application which was initiated by Google Developers as a parcitipant. Graduation in stages Belajar Membuat Aplikasi Android untuk Pemula dan Menjadi Android Developer Expert</p>
+        </table>
+
+        <h4>Gamma Astronomy Club Indonesia</h4>
+        <table>
+            <tr>23 February 2019</tr>
+            <p>In activities Star Party Gamma Astronomy Club as a trainer</p>
+        </table>
+    
+    </article>
+    <article id="card">
+        <h3>Work History</h3>
+        <h4>Contact center</h4>
+        <table>
+            <tr>February 2018 - Current</tr>
+            <p>Universitas Terbuka</p>
+            <ul>
+                <li>Give informations and handle complaints from call, sms, and website</li>
+                <li>Create dialy report</li>
+            </ul>
+        </table>
+    </article>
+    <article id="card">
+        <h3>Skill</h3>
+        <table>
+            <tr>
+                <th>Microsoft Office</th>
+                <td>: Intermediate</td>
+            </tr>
+            <tr>
+                <th>Call Center Opperations</th>
+                <td>: Intermediate</td>
+            </tr>
+            <tr>
+                <th>Data Entry</th>
+                <td>: Intermediate</td>
+            </tr>
+            <tr>
+                <th>Android apps creations</th>
+                <td>: Intermediate</td>
+            </tr>
+            
+        </table>
+    </article>
+ </aside>
+</main>
+
+<footer>
+    <h1 id="catatankaki">Thanks for comming</h1>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+</footer>
+</body>
+<script src="index.js"></script>
+</html>
